d734e032130683d7e62577779d954ad7ad165451
Dmitry Ikryanov <d.ikryanov@gmail.com>
2016-05-12 21:19:21 +0300

    add Navigator pattern

---
+++ app/src/main/java/biz/growapp/baseandroidproject/ui/Navigator.java
@@ -0,0 +1,15 @@
+package biz.growapp.baseandroidproject.ui;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+public final class Navigator {
+
+    private Navigator() {
+        throw new IllegalStateException("Not today!");
+    }
+
+    public static void navigateToSome(@NonNull Context context) {
+//        context.startActivity(new Intent(context, SomeActivity.class));
+    }
+}
